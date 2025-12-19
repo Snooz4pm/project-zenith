484e68e4e718e74da7a5867a376ebd554c350089
youness <Yboumediani06@gmail.com>
2025-12-19 16:26:41 +0100

    Fix: Use @next-auth/prisma-adapter and add prisma generate to build

--- zenithscores-frontend/lib/auth.ts
+++ zenithscores-frontend/lib/auth.ts
@@ -1,6 +1,6 @@
 import GoogleProvider from "next-auth/providers/google"
 import CredentialsProvider from "next-auth/providers/credentials"
-import { PrismaAdapter } from "@auth/prisma-adapter"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import { PrismaClient } from "@prisma/client"
 import bcrypt from "bcrypt"
 import { NextAuthOptions } from "next-auth"
