1a06d25b69ed36039304597341378aeab82f5935
Youness <youness@project-zenith.com>
2026-01-05 22:11:46 +0100

    fix: revert Card.tsx to card.tsx and update imports to lowercase

---
+++ zenithscores-frontend/components/ui/card.tsx
@@ -0,0 +1,18 @@
+import * as React from "react"
+import { cn } from "@/lib/utils"
+
+export const Card = React.forwardRef<
+    HTMLDivElement,
+    React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+    <div
+        ref={ref}
+        className={cn(
+            "rounded-xl border border-white/10 bg-black/40 p-4 backdrop-blur",
+            className
+        )}
+        {...props}
+    />
+))
+
+Card.displayName = "Card"
