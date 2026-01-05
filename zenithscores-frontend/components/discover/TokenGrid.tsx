b102f952752e553291336dfda0556299728fa694
Youness <youness@project-zenith.com>
2026-01-05 22:12:04 +0100

    feat: remove NextAuth API route

--- zenithscores-frontend/components/discover/TokenGrid.tsx
+++ zenithscores-frontend/components/discover/TokenGrid.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState } from 'react';
-import { Card } from '@/components/ui/Card';
+import { Card } from '@/components/ui/card';
 import { Skeleton } from '@/components/ui/Skeleton';
 import Link from 'next/link';
 
