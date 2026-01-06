aba115fb2df8950dcc44b8f5e56f9c702c70bcd9
Youness <youness@project-zenith.com>
2026-01-06 02:10:37 +0100

    fix: remove charts, arena, swap lib - final cleanup

--- zenithscores-frontend/components/charts/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as CandlestickChart } from './CandlestickChart';
-export { default as VolumeChart } from './VolumeChart';
-export { default as MiniChart } from './MiniChart';
-export { default as FullChart } from './FullChart';
